8bef1f6fbc6e3cedb5a8ed2530b279a63556ad3f
Musse Berhane <94893193+M21-BER@users.noreply.github.com>
2024-04-19 16:22:33 +0300

    Update ToolBar.tsx

--- src/components/ToolBar/ToolBar.tsx
+++ src/components/ToolBar/ToolBar.tsx
@@ -14,11 +14,13 @@ import {
   checkmarkCircleOutline,
   informationCircleOutline,
   arrowBackOutline,
+  globeOutline,
 } from "ionicons/icons";
 import { logout } from "../../utils/logout";
 import { useContext } from "react";
 import { UserContext } from "../../context/AuthContext";
 import { Toast } from "../../utils/CustomToast";
+import { Network } from '@capacitor/network';
 
 interface Props {
   title?: string;
@@ -28,47 +30,64 @@ export const ToolBarMain: React.FC<Props> = ({ title,defaultValue }) => {
   const [presentAlert] = useIonAlert();
   const { isAuthed, refresh,navigate,setIsAuthed } = useContext(UserContext);
   const [presentIonToast] = useIonToast();
-  const handleUserToolBar = () => {
-    if (isAuthed) {
-      presentAlert({
-        header: "Sign out!",
-        message: "Are you sure you want to sign out?",
-        backdropDismiss: false,
-        keyboardClose: true,
-        animated: true,
-        buttons: [
-          {
-            text: "Cancel",
-            role: "cancel",
-          },
-          {
-            text: "OK",
-            role: "confirm",
-            handler: async () => {
-              const status: boolean = await logout();
-              if (status) {
-                refresh!();
-                setIsAuthed!(false);
-                navigate!("Login",null,null);
-                Toast(
-                  presentIonToast,
-                  "Sign out successfully!",
-                  checkmarkCircleOutline
-                );
-              } else {
-                Toast(
-                  presentIonToast,
-                  "Sign out failed, please try again later!",
-                  informationCircleOutline
-                );
-              }
-            },
-          },
-        ],
-      });
-    } else {
-      navigate!("Login",null,null);
+  const handleUserToolBar = async() => {
+    if(await logCurrentNetworkStatus()){
+      if (isAuthed) {
+            presentAlert({
+              header: "Sign out!",
+              message: "Are you sure you want to sign out?",
+              backdropDismiss: false,
+              keyboardClose: true,
+              animated: true,
+              buttons: [
+                {
+                  text: "Cancel",
+                  role: "cancel",
+                },
+                {
+                  text: "OK",
+                  role: "confirm",
+                  handler: async () => {
+                    const status: boolean = await logout();
+                    if (status) {
+                      refresh!();
+                      setIsAuthed!(false);
+                      navigate!("Login",null,null);
+                      Toast(
+                        presentIonToast,
+                        "Sign out successfully!",
+                        checkmarkCircleOutline
+                      );
+                    } else {
+                      Toast(
+                        presentIonToast,
+                        "Sign out failed, please try again later!",
+                        informationCircleOutline
+                      );
+                    }
+                  },
+                },
+              ],
+            });
+          } else {
+            navigate!("Login",null,null);
+          }
+    }else{
+      Toast(
+        presentIonToast,
+        "please connect to the internet!",
+        globeOutline
+      );
     }
+    
+  };
+  const logCurrentNetworkStatus = async () => {
+   try {
+    const status = await Network.getStatus();
+    return status.connected;
+   } catch (error) {
+    return false; 
+   }
   };
   return (
     <IonHeader>
